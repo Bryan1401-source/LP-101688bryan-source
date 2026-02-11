7fbe5fe05dda5a4bd273b3c31051775041ebdef1
Bryan1401-source <fxbrian18@gmail.com>
2026-02-11 16:50:59 -0300

    Add files via upload

---
+++ tipagem_dinamica.py
@@ -0,0 +1,8 @@
+# SEM TIPAGEM
+# TIPAGEM DINÂMICA
+
+nome = "Láise"
+idade = 18
+
+print("Nome: ", nome)
+print ("Idade: ", idade)
---
+++ tipagem_estatica.py
@@ -0,0 +1,17 @@
+# TIPAGEM ESTÁTICA
+
+nome: str
+idade: int
+altura: float
+matriculado: bool
+
+nome = "Láise"
+idade = 18
+altura = 1.70
+matriculado = True
+
+
+print ("Nome: ", nome)
+print("Idade ", idade)
+print ("Altura ", altura)
+print ("Matriculado ", matriculado)
